c0cee98a996df73e6dab9e3e667109b64fac3b0c
Wojciech Buława <wbulawa@soldevelo.com>
2018-06-17 16:32:29 +0200

    PD-4: Spring and JBoss integration
    * Added MySQL connection

---
+++ src/main/java/pl/edu/podwozka/podwozkasrv/PodwozkaSrvApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.podwozka.podwozkasrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodwozkaSrvApplication {
+
+    public static void main(String[] args) {
+		SpringApplication.run(PodwozkaSrvApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/edu/podwozka/podwozkasrv/PodwozkaSrvApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.edu.podwozka.podwozkasrv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PodwozkaSrvApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
